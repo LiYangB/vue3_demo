f0a12d3a5227726c39df9036c7e4d9b7e30c5a1a
LiYangBin <2391411037@qq.com>
2023-03-14 20:09:55 +0800

    feat:h函数

--- src/views/Fouth.vue
+++ src/views/Fouth.vue
@@ -6,14 +6,19 @@
     </div>
     <h1>展示element-plug</h1>
     <div>
-         <el-button type="success" round>Success</el-button>
+        <el-button type="success" round>Success</el-button>
+        <hr>
+    </div>
+    <h1>测试h函数</h1>
+    <div>
+        <Btn @on-click="getNum" text="测试h函数"></Btn>
         <hr>
     </div>
 </template>
 
 <script setup lang='ts'>
 import { ElButton } from 'element-plus';
-import { ref, reactive, getCurrentInstance } from 'vue'
+import { ref, reactive, getCurrentInstance, h } from 'vue'
 
 const instance = getCurrentInstance()
 instance?.proxy?.$Loading.show()
@@ -29,14 +34,63 @@ const showLoading = ()=>{
         instance?.proxy?.$Loading.hide()
     }, 5000)
 }
+const testEventLoop = ()=>{
+    async function Prom() {
+        console.log("Y")
+        await Promise.resolve()
+        console.log("X")
+    }
+    const x1 = setTimeout(() => {
+        console.log(1)
+        Promise.resolve().then(()=>{
+            console.log(2)
+        })
+    }, 0);
+    const x2 = setTimeout(() => {
+        console.log(3)
+        Promise.resolve().then(()=>{
+            console.log(4)
+        })
+    }, 0);
+    Promise.resolve().then(() => {
+        console.log(5)
+    })
+    Promise.resolve().then(() => {
+        console.log(6)
+    })
+    Promise.resolve().then(() => {
+        console.log(7)
+    })
+    Promise.resolve().then(() => {
+        console.log(8)
+    })
+    Prom()
+    console.log(0)
+}
 
+type Props = {
+    text: string
+}
+const Btn = (props: Props, ctx: any) => {
+    return h('div', {
+        class: 'bg-[#bfa] w-100px text-center rounded-12px cursor-pointer',
+        onClick: () => {
+            ctx.emit('on-click', 123)
+        }
+    }, props.text)
+}
+
+const getNum = (num: number) => {
+    console.log(num);
+}
 </script>
 
 <style scoped>
-::v-deep.el-button {
-    background: #000;
-}
-/* :deep(.el-button){
+/* ::v-deep .el-button {
     background: #000;
 } */
+:deep(.el-button){
+    background: #bfa;
+    color: red;
+}
 </style>
